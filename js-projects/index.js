329cd6fddc47af9465e94699d482ba6b903c370d
Cécile Grabe <71400566+yabacici@users.noreply.github.com>
2025-04-16 15:31:08 +0200

    checked property

--- js-projects/index.js
+++ js-projects/index.js
@@ -1,20 +1,27 @@
-const decreaseBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBtn");
-const increaseBtn = document.getElementById("increaseBtn");
-const countLabel = document.getElementById("countLabel");
-let count = 0;
+// .checked = property that determines the checked state of an HTML checkbox or radion button element
 
-increaseBtn.onclick = function () {
-  count++;
-  countLabel.textContent = count;
-};
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
 
-decreaseBtn.onclick = function () {
-  count--;
-  countLabel.textContent = count;
-};
+mySubmit.onclick = function () {
+  if (myCheckBox.checked) {
+    subResult.textContent = `you are susbcribed!`;
+  } else {
+    subResult.textContent = `you are NOT subscribed`;
+  }
 
-resetBtn.onclick = function () {
-  count = 0;
-  countLabel.textContent = count;
+  if (visaBtn.checked) {
+    paymentResult.textContent = "you are paying with Visa";
+  } else if (masterCardBtn.checked) {
+    paymentResult.textContent = "you are paying with Mastercard";
+  } else if (payPalBtn.checked) {
+    paymentResult.textContent = "you are paying with PayPal";
+  } else {
+    paymentResult.textContent = "you must select a payment type";
+  }
 };
