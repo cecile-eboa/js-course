60b0d3efde8c72315fad2592ff94a910d4e3f020
Cécile Grabe <71400566+yabacici@users.noreply.github.com>
2025-05-12 16:46:37 +0200

    adding counter programm files

---
+++ counter_program/index.js
@@ -0,0 +1,24 @@
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function () {
+  if (myCheckBox.checked) {
+    subResult.textContent = `you are susbcribed!`;
+  } else {
+    subResult.textContent = `you are NOT subscribed`;
+  }
+  if (visaBtn.checked) {
+    paymentResult.textContent = "you are paying with Visa";
+  } else if (masterCardBtn.checked) {
+    paymentResult.textContent = "you are paying with Mastercard";
+  } else if (payPalBtn.checked) {
+    paymentResult.textContent = "you are paying with PayPal";
+  } else {
+    paymentResult.textContent = "you must select a payment type";
+  }
+};
